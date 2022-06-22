0d7c4f4bad05954d8be6af451a064c84409dac12
Anthony Dunford <trashzy21@gmail.com>
2022-06-22 20:45:18 +0100

    Finish first heuristics version

---
+++ HughBot/Heuristics.fs
@@ -0,0 +1,63 @@
+﻿module Heuristics
+
+open Chess
+open Checkerboard
+open FSharp.Extensions
+
+let private getBaseValue (square: square) : float option =
+    square
+    |> Square.getPiece
+    |> Piece.getValue
+    |> Option.map float
+
+let private knightCentralityValue (square: square) : float =
+    let (i, j) = square.coordinates
+    if i>1 && i<6 then 2.
+    elif i>0 && i<7 then 1.
+    else 0.
+    +
+    if j>1 && j<6 then 2.
+    elif j>0 && j<7 then 1.
+    else 0.
+    
+let private pieceFlexibilityValue (board: board) (square: square) : float =
+    Square.getMoves board square
+    |> List.length |> float
+
+let private pawnAdvanceValue (square: square) : float =
+    let piece = Square.getPiece square
+    let (i,j) = square.coordinates
+    match piece.colour with
+    | White -> 
+        (float j) - 1.
+    | Black ->
+        6. - (float j)
+
+let private staticValueOfSquareOnBoard (board: board) (square: square) : float option =
+    match square.piece with
+    | None -> None
+    | Some piece ->
+        let baseValue = getBaseValue square
+        let flexValue =
+            match piece.pieceType with
+            | Knight -> knightCentralityValue square |> (*) 0.02  |> Some
+            | Bishop -> pieceFlexibilityValue board square |> (*) 0.02 |> Some
+            | Queen -> pieceFlexibilityValue board square |> (*) 0.01 |> Some
+            | Rook -> pieceFlexibilityValue board square |> (*) 0.02 |> Some
+            | Pawn -> pawnAdvanceValue square |> (*) 0.05 |> Some
+            | King -> None
+        Option.map2 (+) baseValue flexValue
+        |> Option.map (fun value ->
+            match piece.colour with
+            | White -> 1.
+            | Black -> -1.
+            |> (*) value 
+        )
+
+let staticValueOfGameState (game: gameState) : float =
+    game.board
+    |> Array2D.fold (fun boardValue square ->
+        match staticValueOfSquareOnBoard game.board square with
+        | None -> boardValue
+        | Some value -> boardValue + value
+    ) 0
