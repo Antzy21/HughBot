a842b0f3468c7a86cc7fe0b722b41f1e2538ccfa
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-06-26 22:05:33 +0100

    Add in trimming of to speed up program

--- HughBot/MinMax.fs
+++ HughBot/MinMax.fs
@@ -2,7 +2,7 @@
 
 open Chess
 
-let rec private minMaxEvaluation (maxDepth: int) (depth: int) (game: gameState) : move option * float =
+let rec private minMaxEvaluation (maxDepth: int) (depth: int) (shallowTrim: int) (deepTrim: int) (deepTrimDepth: int) (game: gameState) : move option * float =
     if depth = maxDepth then
         None, Heuristics.staticEvaluationOfGameState game
     else if GameState.getMovesForPlayer game = List.empty then
@@ -10,11 +10,33 @@ let rec private minMaxEvaluation (maxDepth: int) (depth: int) (game: gameState) 
     else
         let ms = GameState.getMovesForPlayer game
 
+        let newGameStates = 
+            List.map (fun move ->
+                (GameState.makeMove move game, move)
+            ) ms
+
+        let newGsStaticEval = 
+            newGameStates
+            |> List.map (fun (game, move) ->
+                (Heuristics.staticEvaluationOfGameState game, move)
+            ) |>
+            match game.playerTurn with
+            | White -> List.sortByDescending fst
+            | Black -> List.sortBy fst
+        
+        let trim =
+            if depth >= deepTrimDepth then deepTrim
+            else shallowTrim
+
+        let newGsStaticEval = List.take (min newGsStaticEval.Length trim) newGsStaticEval
+
         let movesAndEvals =
-            ms |> List.map (fun move ->
+            newGsStaticEval
+            |> List.map snd
+            |> List.map (fun move ->
                 let evaluation = 
                     GameState.makeMove move game
-                    |> minMaxEvaluation maxDepth (depth + 1)
+                    |> minMaxEvaluation maxDepth shallowTrim deepTrim deepTrimDepth (depth + 1)
                     |> snd
                 (Some move, evaluation)
             )
@@ -26,9 +48,24 @@ let rec private minMaxEvaluation (maxDepth: int) (depth: int) (game: gameState) 
             | Black -> List.sortBy snd
             |> List.head
 
+        if depth < 3 then
+            let move, eval = bestMoveAndEval
+            Option.iter ( 
+                Move.getMoveNotation
+                >> printfn "Turn: %s - Eval : %.2f - Move: %s" (game.playerTurn.ToString()) eval
+            ) move
+
         bestMoveAndEval
 
-let evaluation (depth: int) (game: gameState) : float =
-    let move, eval = minMaxEvaluation depth 0 game
-    Option.iter (Move.getMoveNotation >> printf "%s") move
-    eval
+let evaluation (depth: int) (game: gameState) : move option * float =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    
+    let move, eval = minMaxEvaluation depth 4 2 3 0 game
+    Option.iter (
+        Move.getMoveNotation >> printfn "\n\nEval : %.2f - Move: %s" eval
+    ) move
+    
+    stopWatch.Stop()
+    printfn "Time taken: %.2f" stopWatch.Elapsed.TotalSeconds
+
+    move, eval
