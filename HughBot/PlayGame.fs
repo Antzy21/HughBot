c40e22ddc88a7e78f284c4b30fe4371c81f3b186
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-09-18 21:56:22 +0100

    Allow for different opponent types; test, human, and self play

--- HughBot/PlayGame.fs
+++ HughBot/PlayGame.fs
@@ -1,8 +1,28 @@
 ﻿module PlayGame
 
+open System
+open System.IO
 open FSharp.Extensions
 open Chess
-open System.IO
+
+type Opponent =
+    | Test
+    | Human of string
+    | Computer
+
+let private getUserInputForOpponent () : Opponent =
+    Console.ParseLine "Who am I playing against? Leave blank to test, or 'HughBot'/'HB' to play myself!" (fun (oppString: string) ->
+        match oppString.ToUpper() with
+        | "HUGHBOT" | "HB" -> 
+            Console.WriteLine "I will play against myself!"
+            Some Computer
+        | "" -> 
+            Console.WriteLine "Test game."
+            Some Test
+        | _ -> 
+            Console.WriteLine $"Good luck {oppString}!"
+            Some (Human oppString)
+    )
 
 let private getUserInputMoveFromNotation (game: gameState) (moves: move list) : move option =
     Console.ParseLineWithBreakOption "Please enter move" (fun (notation: string) ->
@@ -21,17 +41,45 @@ let private getUserInputMoveFromList (moves: move list) =
         )
     )
 
+let private getComputerMove (game: gameState) (file: StreamWriter): move =
+    printfn "\nCalculating move...\n"
+    let move, _ =
+        try
+            MinMax.evaluation game
+        with
+        | ex ->
+            GameState.print game
+            file.WriteLine($"{ex}")
+            file.WriteLine($"{GameState.toFen game}")
+            file.Close()
+            failwith $"{ex}" 
+    move |> Option.get
+
+let private getOpponentMove (game: gameState) (opponent: Opponent) (file: StreamWriter): move =
+    match opponent with
+    | Computer -> getComputerMove game file
+    | _ ->
+        let moves = GameState.getMoves game
+        getUserInputMoveFromNotation game moves
+        |> Option.defaultWith (fun () -> getUserInputMoveFromList moves)
+
 let play (gs: gameState) =
     
     let mutable game = gs
     printfn "I'm HughBot, a chess engine. Let's play!"
-    printfn "Who am I playing against? (Leave blank to test - No log will be created)"
-    let opponent = System.Console.ReadLine ()
-    let botColour = Console.ParseLine "Please select White or Black for bot to play" Colour.tryParse 
+    let opponent = getUserInputForOpponent ()
+    let botColour = 
+        match opponent with
+        | Computer -> White
+        | _ -> Console.ParseLine "Please select White or Black for me to play" Colour.tryParse 
     
     let date = System.DateTime.Now.ToString("ddMMyyyy")
-    let fileName = $"Hughbot({botColour |> Colour.toChar})vs({botColour |> Colour.opposite |> Colour.toChar}){opponent}_{date}.log"
-    let path = $"../../../RecordedGames/{fileName}"
+    let fileName = 
+        match opponent with
+        | Human name -> $"Hughbot({botColour |> Colour.toChar})vs({botColour |> Colour.opposite |> Colour.toChar}){name}"
+        | Computer -> $"SelfPlay"
+        | Test -> ""
+    let path = $"../../../RecordedGames/{fileName}_{date}.log"
     let file = File.CreateText(path)
 
     let mutable moveList : string list = []
@@ -39,22 +87,9 @@ let play (gs: gameState) =
     while GameState.isGameOver game |> not do
         let currentMove =
             if game.playerTurn = botColour then
-                printfn "Calculating move..."
-                let move, _ =
-                    try
-                        MinMax.evaluation game
-                    with
-                    | ex ->
-                        GameState.print game
-                        file.WriteLine($"{ex}")
-                        file.WriteLine($"{GameState.toFen game}")
-                        file.Close()
-                        failwith $"{ex}" 
-                move |> Option.get
+                getComputerMove game file
             else
-                let moves = GameState.getMoves game
-                getUserInputMoveFromNotation game moves
-                |> Option.defaultWith (fun () -> getUserInputMoveFromList moves)
+                getOpponentMove game opponent file
         file.WriteLine($"{Move.getMoveNotation currentMove}")
         moveList <- List.append moveList [Move.getMoveNotation currentMove]
         game <- GameState.Update.makeMove currentMove game
@@ -69,7 +104,7 @@ let play (gs: gameState) =
 
     printfn "\nGood game!"
 
-    if opponent = "" then
+    if opponent = Test then
         File.Delete path
 
 let newGame () =
