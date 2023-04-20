7c6205f2cbf36c81cadbbdb917d135fcc2de8fd9
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-20 11:05:25 +0100

    Use Algorithms.minMax

--- HughBot/MinMax.fs
+++ HughBot/MinMax.fs
@@ -4,19 +4,21 @@ open Chess
 open SearchAlgorithms
 
 /// From a gievn game, get the moves available and calculate the heuristic value if the move is applied.
-let private getMovesAndEvaluationPairs (gameState: gameState) : moveAndEvaluation<move, float> list =
+let private getMovesAndNewGameStates (gameState: gameState) : (move * gameState) list =
     GameState.getMoves gameState
     |> List.map (fun move ->
         let newGs = GameState.Update.makeMove move gameState
-        let eval = Heuristics.staticEvaluationOfGameState newGs
-        {move = Some move; eval = eval}
+        move, newGs
     )
 
 /// Orders the list of moves based on evaluation desired by player of move
-let private orderMoves (gameState: gameState) (moveEvalPairs: moveAndEvaluation<move, float> list) : moveAndEvaluation<move, float> list =
-    match gameState.playerTurn with
-    | White -> moveEvalPairs |> List.sortByDescending (fun mep -> mep.eval)
-    | Black -> moveEvalPairs |> List.sortByDescending (fun mep -> mep.eval)
+let private orderMovesAndGsByStaticEval (isMaxing: bool) : (move * gameState) list -> (move * gameState) list =
+    fun (move, gameState) -> Heuristics.staticEvaluationOfGameState gameState
+    |>
+    if isMaxing then
+        List.sortByDescending
+    else
+        List.sortBy
 
 let private printEval gameState depth move staticEvaluation =
     MoveParser.FullNotation.toString gameState.board move
@@ -25,42 +27,18 @@ let private printEval gameState depth move staticEvaluation =
         (gameState.playerTurn.ToString()) staticEvaluation 
     |> printfn "%s"
 
-let rec private minMaxEvaluation (depth: int) (gameState: gameState) : move option * float =
-    if depth = 0 then
-        None, Heuristics.staticEvaluationOfGameState gameState
-    else if GameState.getMoves gameState = List.empty then
-        None, Heuristics.gameOverEvaluation depth gameState
-    else
-        let orderedTrimmedMovesAndEvals =
-            getMovesAndEvaluationPairs gameState
-            |> orderMoves gameState
+let private getNodesFromParent (gameState: gameState) =
+    getMovesAndNewGameStates gameState
+    |> orderMovesAndGsByStaticEval (Colour.toBool gameState.playerTurn)
 
-        let movesAndEvals =
-            orderedTrimmedMovesAndEvals
-            |> List.map (fun moveAndEval ->
-                if depth > 1 then
-                    printEval gameState depth moveAndEval.move.Value moveAndEval.eval
-                let newGs = GameState.Update.makeMove moveAndEval.move.Value gameState
-                let evaluation = 
-                    newGs
-                    |> minMaxEvaluation (depth - 1)
-                    |> snd
-                (moveAndEval.move, evaluation)
-            )
+let private evaluationFunction move gameState = Heuristics.staticEvaluationOfGameState gameState
 
-        let bestMoveAndEval = 
-            movesAndEvals |>
-            match gameState.playerTurn with
-            | White -> List.sortByDescending snd
-            | Black -> List.sortBy snd
-            |> List.head
-
-
-        bestMoveAndEval
+let private chessMinMax = Algorithms.minMax getNodesFromParent evaluationFunction
 
 let evaluation (game: game) : move option * float =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    let move, eval = minMaxEvaluation 4 game.gameState
+    let isMaxing = Colour.toBool game.gameState.playerTurn
+    let move, eval = chessMinMax 2 isMaxing None game.gameState
     Option.iter (
         MoveParser.FullNotation.toString game.gameState.board >> printfn "\n\nEval : %.2f - Move: %s" eval
     ) move
