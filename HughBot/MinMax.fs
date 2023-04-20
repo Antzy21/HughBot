f717eb777aca8153ebde59d34c8a20de711d326f
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-20 08:47:36 +0100

    Use gamestate in functions instead of passing game

--- HughBot/MinMax.fs
+++ HughBot/MinMax.fs
@@ -4,43 +4,43 @@ open Chess
 open SearchAlgorithms
 
 /// From a gievn game, get the moves available and calculate the heuristic value if the move is applied.
-let private getMovesAndEvaluationPairs (game: game) : moveAndEvaluation<move, float> list =
-    GameState.getMoves game.gameState
+let private getMovesAndEvaluationPairs (gameState: gameState) : moveAndEvaluation<move, float> list =
+    GameState.getMoves gameState
     |> List.map (fun move ->
-        let newGs = Game.Update.makeMove move game
-        let eval = Heuristics.staticEvaluationOfGameState newGs.gameState
+        let newGs = GameState.Update.makeMove move gameState
+        let eval = Heuristics.staticEvaluationOfGameState newGs
         {move = Some move; eval = eval}
     )
 
 /// Orders the list of moves based on evaluation desired by player of move
-let private orderMoves (game: game) (moveEvalPairs: moveAndEvaluation<move, float> list) : moveAndEvaluation<move, float> list =
-    match game.gameState.playerTurn with
+let private orderMoves (gameState: gameState) (moveEvalPairs: moveAndEvaluation<move, float> list) : moveAndEvaluation<move, float> list =
+    match gameState.playerTurn with
     | White -> moveEvalPairs |> List.sortByDescending (fun mep -> mep.eval)
     | Black -> moveEvalPairs |> List.sortByDescending (fun mep -> mep.eval)
 
-let private printEval game depth move staticEvaluation =
-    MoveParser.FullNotation.toString game.gameState.board move
+let private printEval gameState depth move staticEvaluation =
+    MoveParser.FullNotation.toString gameState.board move
     |> sprintf "%s Turn: %d %s - Eval : %.2f - Move: %s"
-        (String.replicate depth "  ") (game.gameState.fullMoveClock)
-        (game.gameState.playerTurn.ToString()) staticEvaluation 
+        (String.replicate depth "  ") (gameState.fullMoveClock)
+        (gameState.playerTurn.ToString()) staticEvaluation 
     |> printfn "%s"
 
-let rec private minMaxEvaluation (depth: int) (game: game) : move option * float =
+let rec private minMaxEvaluation (depth: int) (gameState: gameState) : move option * float =
     if depth = 0 then
-        None, Heuristics.staticEvaluationOfGameState game.gameState
-    else if GameState.getMoves game.gameState = List.empty then
-        None, Heuristics.gameOverEvaluation depth game.gameState
+        None, Heuristics.staticEvaluationOfGameState gameState
+    else if GameState.getMoves gameState = List.empty then
+        None, Heuristics.gameOverEvaluation depth gameState
     else
         let orderedTrimmedMovesAndEvals =
-            getMovesAndEvaluationPairs game
-            |> orderMoves game
+            getMovesAndEvaluationPairs gameState
+            |> orderMoves gameState
 
         let movesAndEvals =
             orderedTrimmedMovesAndEvals
             |> List.map (fun moveAndEval ->
                 if depth > 1 then
-                    printEval game depth moveAndEval.move.Value moveAndEval.eval
-                let newGs = Game.Update.makeMove moveAndEval.move.Value game
+                    printEval gameState depth moveAndEval.move.Value moveAndEval.eval
+                let newGs = GameState.Update.makeMove moveAndEval.move.Value gameState
                 let evaluation = 
                     newGs
                     |> minMaxEvaluation (depth - 1)
@@ -50,7 +50,7 @@ let rec private minMaxEvaluation (depth: int) (game: game) : move option * float
 
         let bestMoveAndEval = 
             movesAndEvals |>
-            match game.gameState.playerTurn with
+            match gameState.playerTurn with
             | White -> List.sortByDescending snd
             | Black -> List.sortBy snd
             |> List.head
@@ -60,7 +60,7 @@ let rec private minMaxEvaluation (depth: int) (game: game) : move option * float
 
 let evaluation (game: game) : move option * float =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    let move, eval = minMaxEvaluation 4 game
+    let move, eval = minMaxEvaluation 4 game.gameState
     Option.iter (
         MoveParser.FullNotation.toString game.gameState.board >> printfn "\n\nEval : %.2f - Move: %s" eval
     ) move
